bbca357b8ace3abad03f49476a0348c80b42a879
Ilya Lapan <lapan.ilya@gmail.com>
2017-02-15 21:42:55 +0000

    Fixed the None in value bug

--- start_scraping.py
+++ start_scraping.py
@@ -25,7 +25,7 @@ def get_relevant_logs():
         f_logs = open('logs', 'r')
         all_logs = f_logs.readlines()
         current_time = time.time()
-        limit_timestamp = current_time - time_limit * 60 * 60 * 1000
+        limit_timestamp = current_time - time_limit * 60 * 60
         for log in all_logs:
             if float(str.split(log, ',')[0]) > limit_timestamp:
                 logs.append(log)
@@ -36,6 +36,7 @@ def calc_total_input(transaction):
     total_input = 0
     for bitcoin_input in transaction['out']:
         total_input += bitcoin_input['value']
+    return total_input
 
 
 def parse_transactions_in_block(block):
@@ -47,49 +48,76 @@ def parse_transactions_in_block(block):
 
 
 def main():
+    running = True
+    while running:
+        logs = get_relevant_logs()
 
-    logs = get_relevant_logs()
+        f_logs = open('logs', 'w')
+        if logs:
+            for log_item in logs:
+                f_logs.write(str(log_item))  # write only relevant logs in to the file to keep it small
 
-    f_logs = open('logs', 'w')
-    if logs:
-        for log_item in logs:
-            f_logs.write(str(log_item))  # write only relevant logs in to the file to keep it small
-
-    # get the hash that script stopped at last time, or start from the beginning
-    if os.path.isfile('current_hash'):
-        f_current_hash = open('current_hash', 'r')
-        current_hash = f_current_hash.read()
-        if not current_hash:
+        # get the hash that script stopped at last time, or start from the beginning
+        if os.path.isfile('current_hash'):
+            f_current_hash = open('current_hash', 'r')
+            current_hash = f_current_hash.read()
+            if not current_hash:
+                current_hash = starting_hash
+            f_current_hash.close()
+        else:
             current_hash = starting_hash
-        f_current_hash.close()
-    else:
-        current_hash = starting_hash
 
-    if not os.path.exists('data'):
-        os.makedirs('data')
+        if not os.path.exists('data'):
+            os.makedirs('data')
 
-    for i in range(len(logs)*requests_per_log, maximum_requests):  # to stay in max requests range
+        print str(len(logs)*requests_per_log) + 'requests in last 8 hours'
+        print 'starting scraping'
 
-        r = requests.get(url+current_hash)
-        block = ujson.loads(r.text)
-        block_timestamp_str = format_timestamp(block['time'])
-        block_index_str = str(block['block_index'])
+        for i in range(len(logs)*requests_per_log, maximum_requests):  # to stay in max requests range
+            attempts = 0
+            #try 10 times, after 10nth time stop
+            while attempts < 10:
+                try:
+                    r = requests.get(url+current_hash)
+                except:
+                    print url+current_hash
+                    print attempts
+                    attempts += 1
+                else:
+                    break
+            if attempts > 9:
+                print "had an issue here, skipping"
+                continue
+            try:
+                block = ujson.loads(r.text)
+            except ValueError:
+                print r.text
+                print url+current_hash
+                print attempts
+                continue
 
-        # only log each 20th request
-        if i % 20 == 0:
-            f_logs.write(str(time.time())+'\n')  # log the time when the request was made
-        f = open('data/'+block_index_str+"__"+block_timestamp_str, "w+")
-        block_string = parse_transactions_in_block(block)
-        current_hash = block['prev_block']
-        f.write(block_string)
-        f.close()
+            block_timestamp_str = format_timestamp(block['time'])
+            block_index_str = str(block['block_index'])
 
-        f_current_hash = open('current_hash', 'w')
-        # record the hash the script stopped at
-        f_current_hash.write(current_hash)
-        f_current_hash.close()
+            # only log each 20th request
+            if i % 20 == 0:
+                f_logs.write(str(time.time())+'\n')  # log the time when the request was made
+            f = open('data/'+block_index_str+"__"+block_timestamp_str, "w+")
+            block_string = parse_transactions_in_block(block)
+            current_hash = block['prev_block']
+            f.write(block_string)
+            f.close()
 
-    f_logs.close()
+            f_current_hash = open('current_hash', 'w')
+            # record the hash the script stopped at
+            f_current_hash.write(current_hash)
+            f_current_hash.close()
+
+        f_logs.close()
+        if block['time'] < 1451600000:
+            running = False
+
+    print "finished scarping"
 
 if __name__ == "__main__":
     main()
